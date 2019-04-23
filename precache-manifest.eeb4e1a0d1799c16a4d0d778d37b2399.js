f0a4c42e3da86030b20608da14cfe85559ca6eb5
OlegGre4ka <gre4kae@ukr.net>
2019-04-23 12:37:16 +0300

    Updates

---
+++ precache-manifest.eeb4e1a0d1799c16a4d0d778d37b2399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bf556a8367ddef8b1fa",
+    "url": "/React-Films-App/static/css/main.52d72347.chunk.css"
+  },
+  {
+    "revision": "8bf556a8367ddef8b1fa",
+    "url": "/React-Films-App/static/js/main.5d4005ba.chunk.js"
+  },
+  {
+    "revision": "40b8d03173594d3f2570",
+    "url": "/React-Films-App/static/js/runtime~main.8f8433da.js"
+  },
+  {
+    "revision": "1906e479058a459b2e86",
+    "url": "/React-Films-App/static/css/2.5d834e36.chunk.css"
+  },
+  {
+    "revision": "1906e479058a459b2e86",
+    "url": "/React-Films-App/static/js/2.3d451eae.chunk.js"
+  },
+  {
+    "revision": "33b0c3b45e601987258f01d718e4ce80",
+    "url": "/React-Films-App/index.html"
+  }
+];
